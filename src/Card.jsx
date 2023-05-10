cfcc04231a6ab47e938b483fe60c28cde25f3e01
Harry Fleming <email@harryfleming.net>
2023-05-10 10:53:00 +0100

    implement cascading card flips

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from 'react'
+
 import './Card.css'
 
-export default function Card({char, flipped, frontStyle, backStyle}) {
+export default function Card({char, flipIndex, frontStyle, backStyle}) {
+
+  const [flipped, setFlipped] = useState('')
+
+  if (backStyle) {
+    useEffect(() => {
+      setTimeout(() => {
+        setFlipped('card-flipped')
+      }, 100 * flipIndex)
+    }, [])
+  }
+
   return <>
   <div className={`card ${flipped}`}>
     <div className={`back ${backStyle}`}>{char}</div>
--- src/RowChecked.jsx
+++ src/RowChecked.jsx
@@ -5,18 +5,10 @@ import Card from './Card'
 
 export default function RowChecked({ checkedGuess }) {
 
-  const [side, setSide] = useState('')
-
-  useEffect(() => {
-    setTimeout(() => {
-      setSide('card-flipped')
-    }, 250)
-  }, [])
-
   return <>
   <div className="row">
     {checkedGuess.map((checkedChar, i) => {
-      return <Card key={i} char={checkedChar.char} flipped={`${side}`} frontStyle={'blank filled'} backStyle={`${checkedChar.match}`} />
+      return <Card key={i} char={checkedChar.char} flipIndex={i} frontStyle={'blank filled'} backStyle={`${checkedChar.match}`} />
     })}
   </div>
   </>
