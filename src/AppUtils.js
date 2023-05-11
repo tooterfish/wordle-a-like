071173391e3789522f91e14ec56bffa0a01f07aa
Harry Fleming <email@harryfleming.net>
2023-05-11 18:07:11 +0100

    implement splitmix32 PRNG for use in word of the day

--- src/AppUtils.js
+++ src/AppUtils.js
@@ -59,3 +59,12 @@ function binarySearch(array, value) {
   }
   return -1
 }
+
+function splitmix32(a) {
+  return function() {
+    a |= 0; a = a + 0x9e3779b9 | 0;
+    var t = a ^ a >>> 16; t = Math.imul(t, 0x21f0aaad);
+        t = t ^ t >>> 15; t = Math.imul(t, 0x735a2d97);
+    return ((t = t ^ t >>> 15) >>> 0) / 4294967296;
+  }
+}
