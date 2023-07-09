9cfcb97f887e1dc7fc92651c26f5922201d7375b
Harry Fleming <email@harryfleming.net>
2023-07-09 12:49:04 +0100

    add small delay to flip animation to prevent bug in firefox where first flip would not play

--- src/Card.jsx
+++ src/Card.jsx
@@ -10,7 +10,7 @@ export default function Card({char, flipIndex, frontStyle, backStyle}) {
     useEffect(() => {
       setTimeout(() => {
         setFlipped('card-flipped')
-      }, 100 * flipIndex)
+      }, 100 * flipIndex + 100)
     }, [])
   }
 
