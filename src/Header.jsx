a9d9f1760f9ec229f32cdfec86439ae4eab404ce
Harry Fleming <email@harryfleming.net>
2023-11-07 16:46:14 +0000

    styling for options buttons

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,8 +2,12 @@ import './Header.css'
 
 export default function Header({openHowTo}) {
   return <div className="header">
+    <div className='header-title'>
     <h1>Wordle-a-Like</h1>
-    <button onClick={openHowTo}>HowTo</button>
+    </div>
+    <div className='header-options'>
+      <button className='options-button' onClick={openHowTo}>?</button>
+    </div>
   </div>
   
 }
